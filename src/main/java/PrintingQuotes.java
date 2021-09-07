1702947e24727be462093d80a6974089ed0486e5
fpfrances <fpfrances@hotmail.com>
2021-09-06 21:13:49 -0400

    Initial commit

---
+++ src/main/java/PrintingQuotes.java
@@ -0,0 +1,16 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Filipe Frances
+ */
+import java.util.Scanner;
+public class PrintingQuotes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("What is the quote?");
+        String quote = sc.nextLine();
+        System.out.print("Who said it?");
+        String author = sc.nextLine();
+        System.out.println(author+ " says, \""+quote+"\"");
+    }
+}
+
